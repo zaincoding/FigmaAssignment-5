6c1a6c6d7e1ba5b7713443b4289682aabbbdd493
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-11-06 14:21:16 +0500

    Update page.tsx  left margin

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ text-white  p-[10px] rounded-[10px]'>
 
     </div>
 
-    <div className="w-1/2 my-10 ml-[400px]">
+    <div className="w-1/2 my-10 ml-[300px]">
 
     <Image
       src="images/rs-group-wrap ⏵ rs-group.svg"
